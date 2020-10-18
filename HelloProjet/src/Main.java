a7088cca1f89ee8270f734fb757d0dac0284174c
Yring <xiang119zhen@163.com>
2020-10-18 20:34:04 +0800

    ssh提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("这老师是不是讲太急了，感觉没说清楚就跳下一个了");
+		System.out.println("使用同步里面的ssh来上传项目");
     }
 }
